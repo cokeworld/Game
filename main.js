20588e857049fbccd68260da59c1449e2fbef8fa
Tony Kim <67273859+cokeworld@users.noreply.github.com>
2020-07-31 17:27:06 +0900

    JS: start game

--- main.js
+++ main.js
@@ -1,11 +1,69 @@
-const gameBtn = document.querySelector(".game__button");
+"use strict";
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
-const CARROT_SIZE = 80;
+const gameBtn = document.querySelector(".game__button");
+const gameTimer = document.querySelector(".game__timer");
+const gameScore = document.querySelector(".game__score");
+
+const popUp = document.querySelector(".pop-up");
+const refreshBtn = document.querySelector(".pop-up__refresh");
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener("click", () => {
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function startGame() {
+  initGame();
+  showStopBtn();
+  showTimerAndScore();
+  startGameTimer();
+}
+
+function stopGame() {
+  showPlayBtn();
+}
+
+function showStopBtn() {
+  const icon = document.querySelector(".fa-play");
+  icon.classList.add("fa-stop");
+  icon.classList.remove("fa-play");
+}
+
+function showPlayBtn() {
+  const icon = document.querySelector(".fa-stop");
+  icon.classList.add("fa-play");
+  icon.classList.remove("fa-stop");
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = "visible";
+  gameScore.style.visibility = "visible";
+}
+
+function startGameTimer() {
+  const setTimer = window.setTimeout(() => {
+    
+  }, timeout);
+}
+
 function initGame() {
-  console.log(fieldRect);
-  addItem("carrot", 5, "img/carrot.png");
-  addItem("bug", 5, "img/bug.png");
+  field.innerHTML = "";
+  gameScore.innerHTML = CARROT_COUNT;
+  addItem("carrot", CARROT_COUNT, "img/carrot.png");
+  addItem("bug", BUG_COUNT, "img/bug.png");
 }
 
 function addItem(className, count, imagePath) {
@@ -31,4 +89,7 @@ function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-initGame();
+refreshBtn.addEventListener("click", () => {
+  item.remove();
+  initGame();
+});
